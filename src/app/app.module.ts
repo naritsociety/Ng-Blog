4c8b4edf7a1e86ccfc87c6d3d3f3de9fa0d29694
Narit Charoenval <naritsociety@gmail.com>
2022-04-06 09:57:22 +0700

    Add contact link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,8 +16,7 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     CoreModule,
     SharedModule,
-    ContactModule,
-    RouterModule
+    ContactModule
   ],
   providers: [],
   bootstrap: [AppComponent]
